cd7920bbd6d650f8f51f099fff878a29b2c70413
Zhang Xuan <xuan.zhang@xiaobao100.com>
2018-12-27 11:45:11 +0800

    store sync update; docs update

--- src/store.ts
+++ src/store.ts
@@ -13,9 +13,10 @@ type MappedDirector<T extends Record<string, Actor>> = {
   [K in keyof T]?: (...args: Parameters<T[K]>) => ReturnType<ReturnType<T[K]>>
 }
 
-interface Connected {
-  $sync(state: Record<string, any>): void
+interface Connected extends ConnectedPage {
+  $sync(state: Record<string, any>, cb?: () => void): void
 }
+
 function isConnected(page: any): page is Connected {
   return typeof page.$sync === 'function'
 }
@@ -28,6 +29,17 @@ function fix<T extends Record<string, any>>(data: T): T {
     return o
   }, {})
 }
+function shallowEql(objA: Record<string, any>, objB: Record<string, any>): boolean {
+  if (objA === objB) {
+    return true
+  }
+  const keysA = Object.keys(objA)
+  const keysB = Object.keys(objB)
+  if (keysA.length !== keysB.length) {
+    return false
+  }
+  return Array.from(new Set([...keysA, ...keysB])).every(k => objA[k] === objB[k])
+}
 
 export interface Action {
   type: string
@@ -35,13 +47,42 @@ export interface Action {
 }
 
 export type Reducer<T extends Record<string, any>> = (state: T, action: Action) => T
-export type Dispatch = (data: Action) => void
+export type Dispatch = (action: Action) => void
 export type Director<T extends Record<string, Function>> = (dispatch: Dispatch) => T
 
+export interface ConnectedPage {
+  /**
+   * 停止接收被动同步
+   */
+  $stopSync?: boolean
+  /**
+   * 主动同步订阅的 state
+   *
+   * @param cb 完成后回调
+   */
+  $sync?(cb?: () => void): void
+}
+
 export interface Store<T extends Record<string, any>> {
+  /**
+   * 状态
+   */
   state: T
+  /**
+   * 处理状态的 Reducer
+   */
   reducer: Reducer<T>
-  dispatch(data: Action): void
+  /**
+   * 触发一个 Action
+   *
+   * @param action 移交给 Reducer 的 Action
+   */
+  dispatch(action: Action): void
+  /**
+   * 更新状态
+   *
+   * @param state 新状态
+   */
   setState(state: Partial<T>): void
 }
 
@@ -89,19 +130,21 @@ export function createStore<T extends Record<string, any>>(reducer: Reducer<T>):
   return {
     state: {} as T,
     reducer,
-    dispatch(data: Action): void {
-      const state = this.reducer(this.state, data)
+    dispatch(action: Action): void {
+      const state = this.reducer(this.state, action)
       this.setState(state)
     },
     setState(state: Partial<T>): void {
-      Object.assign(this.state, state)
-      getCurrentPages().forEach(p => isConnected(p) && p.$sync(this.state))
+      if (!shallowEql(this.state, state)) {
+        Object.assign(this.state, state)
+        getCurrentPages().forEach(p => isConnected(p) && !p.$stopSync && p.$sync(this.state))
+      }
     }
   }
 }
 
 /**
- * 创建一个关联指定 Store 的 App.
+ * 创建一个关联指定 Store 的 App
  *
  * @param store 关联的 Store
  * @returns App 构造器
@@ -144,7 +187,7 @@ export function connect(): <
 export function connect<TState extends Record<string, any>>(
   stateMapper: StateMapper<TState>
 ): <TData extends Record<string, any>, TPage extends wx.Page & Record<string, any>>(
-  options: wx.Page<TData & Partial<TState>> & TPage
+  options: wx.Page<TData & Partial<TState>> & TPage & ConnectedPage
 ) => void
 /**
  * 创建一个关联默认 Provider 的 Page
@@ -160,26 +203,39 @@ export function connect<
   stateMapper: StateMapper<TState>,
   director: Director<TDirector>
 ): <TData extends Record<string, any>, TPage extends wx.Page & Record<string, any>>(
-  options: wx.Page<TData & Partial<TState>> & TPage & MappedDirector<TDirector>
+  options: wx.Page<TData & Partial<TState>> & TPage & ConnectedPage & MappedDirector<TDirector>
 ) => void
 
 export function connect(stateMapper?: StateMapper<any>, director?: Director<any>): any {
   return <TData extends Record<string, any>, TPage extends wx.Page & Record<string, any>>(
-    options: wx.Page<TData> & TPage
+    options: wx.Page<TData> & TPage & ConnectedPage
   ): void => {
     if (stateMapper) {
       const { onLoad } = options || ({} as any)
       options = {
         ...options,
-        $sync(state: any): void {
-          this.setData(fix(stateMapper(state)))
-        },
-        onLoad(query?: any): void {
-          const app = getApp<{ store: Store<any> }>()
-          this.$sync(app.store.state)
-          if (onLoad) {
-            return onLoad.call(this, query)
+        $sync(state?: any, cb?: any): void {
+          if (typeof state === 'function') {
+            cb = state
+            state = null
           }
+          if (state == null) {
+            const app = getApp<{ store: Store<any> }>()
+            state = app.store.state
+          }
+          const data = fix(stateMapper(state))
+          if (Object.keys(data).length > 0) {
+            this.setData(data, cb)
+          } else if (cb) {
+            cb()
+          }
+        },
+        onLoad(this: Connected, query?: any): void {
+          this.$sync(() => {
+            if (onLoad) {
+              onLoad.call(this, query)
+            }
+          })
         }
       }
     }
@@ -191,7 +247,7 @@ export function connect(stateMapper?: StateMapper<any>, director?: Director<any>
           const app = getApp<{ store: Store<any> }>()
           Object.assign(this, director(app.store.dispatch.bind(app.store)))
           if (onLoad) {
-            return onLoad.call(this, query)
+            onLoad.call(this, query)
           }
         }
       }
--- weuse.d.ts
+++ weuse.d.ts
@@ -1043,12 +1043,40 @@ declare namespace weuse {
       [x: string]: any
     }
     type Reducer<T extends Record<string, any>> = (state: T, action: Action) => T
-    type Dispatch = (data: Action) => void
+    type Dispatch = (action: Action) => void
     type Director<T extends Record<string, Function>> = (dispatch: Dispatch) => T
+    interface ConnectedPage {
+      /**
+       * 停止接收被动同步
+       */
+      $stopSync?: boolean
+      /**
+       * 主动同步订阅的 state
+       *
+       * @param cb 完成后回调
+       */
+      $sync?(cb?: () => void): void
+    }
     interface Store<T extends Record<string, any>> {
+      /**
+       * 状态
+       */
       state: T
+      /**
+       * 处理状态的 Reducer
+       */
       reducer: Reducer<T>
-      dispatch(data: Action): void
+      /**
+       * 触发一个 Action
+       *
+       * @param action 移交给 Reducer 的 Action
+       */
+      dispatch(action: Action): void
+      /**
+       * 更新状态
+       *
+       * @param state 新状态
+       */
       setState(state: Partial<T>): void
     }
     /**
@@ -1075,7 +1103,7 @@ declare namespace weuse {
      */
     function createStore<T extends Record<string, any>>(reducer: Reducer<T>): Store<T>
     /**
-     * 创建一个关联指定 Store 的 App.
+     * 创建一个关联指定 Store 的 App
      *
      * @param store 关联的 Store
      * @returns App 构造器
@@ -1103,7 +1131,7 @@ declare namespace weuse {
     function connect<TState extends Record<string, any>>(
       stateMapper: StateMapper<TState>
     ): <TData extends Record<string, any>, TPage extends wx.Page & Record<string, any>>(
-      options: wx.Page<TData & Partial<TState>> & TPage
+      options: wx.Page<TData & Partial<TState>> & TPage & ConnectedPage
     ) => void
     /**
      * 创建一个关联默认 Provider 的 Page
@@ -1116,7 +1144,7 @@ declare namespace weuse {
       stateMapper: StateMapper<TState>,
       director: Director<TDirector>
     ): <TData extends Record<string, any>, TPage extends wx.Page & Record<string, any>>(
-      options: wx.Page<TData & Partial<TState>> & TPage & MappedDirector<TDirector>
+      options: wx.Page<TData & Partial<TState>> & TPage & ConnectedPage & MappedDirector<TDirector>
     ) => void
   }
   const request: request
